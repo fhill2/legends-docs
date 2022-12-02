b0982b87de51fa8cf745fdb4874c1af378896d6b
Freddie Hill <freddiehill000@gmail.com>
2022-12-02 18:12:16 +0000

    add emoji support to html and pdf

--- tools/server.js
+++ tools/server.js
@@ -2,25 +2,21 @@ const liveServer = require("live-server");
 const { execSync } = require("child_process");
 const Path = require("path");
 var hasbin = require("hasbin");
-
 // cwd when called from npm run dev -> node tools/server.js
 // process.cwd() -> git root
 // __dirname /tools/
-
 hasbin("pandoc", function (result) {
   if (!result) {
     console.log("pandoc not found on PATH - quitting..");
     process.exit();
   }
 });
-
-hasbin("weasyprint", function (result) {
+hasbin("prince", function (result) {
   if (!result) {
-    console.log("weasyprint not found on PATH - quitting..");
+    console.log("prince not found on PATH - quitting..");
     process.exit();
   }
 });
-
 // https://github.com/tapio/live-server/issues/340
 function buildExec(cmd) {
   execSync(
@@ -37,9 +33,7 @@ function buildExec(cmd) {
     }
   );
 }
-
 pandocCmd = "pandoc --verbose --defaults .gen_shared.yml";
-
 function build_markdown_file(path) {
   // Generate Individual File - HTML
   console.log(
@@ -51,7 +45,7 @@ function build_markdown_file(path) {
   console.log(
     `========== Converting ${path.name}.html --> ${path.name}.pdf ==========`
   );
-  buildExec(`prince ${path.name}.html ${path.name}.pdf`);
+  buildExec(`prince -o ${path.name}.pdf ${path.name}.html`);
 
   // as --include-before-body inputs an HTML file and not MD, ensure the header file exists as HTML
   console.log(`========== Generating Header for Report.html ==========`);
